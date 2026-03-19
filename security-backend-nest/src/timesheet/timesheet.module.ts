5d6a922073f20174f1e2e4ffbe8828b5859ff24e
Fahad Jibran <fahadjibran1@gmail.com>
2026-03-19 11:51:06 +0000

    Fix timesheet module dependencies for deploy

--- security-backend-nest/src/timesheet/timesheet.module.ts
+++ security-backend-nest/src/timesheet/timesheet.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Timesheet } from './entities/timesheet.entity';
 import { TimesheetController } from './timesheet.controller';
 import { TimesheetService } from './timesheet.service';
+import { CompanyModule } from '../company/company.module';
+import { GuardProfileModule } from '../guard-profile/guard-profile.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Timesheet])],
+  imports: [TypeOrmModule.forFeature([Timesheet]), CompanyModule, GuardProfileModule],
   controllers: [TimesheetController],
   providers: [TimesheetService],
   exports: [TimesheetService, TypeOrmModule]
