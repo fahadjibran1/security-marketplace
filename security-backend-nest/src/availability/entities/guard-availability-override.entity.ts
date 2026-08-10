983ff98e5abc722d3b365e360cd2135994941806
Fahad Jibran <fahadjibran@gmail.com>
2026-08-10 13:09:58 +0100

    Make availability override guard relation non-nullable

--- security-backend-nest/src/availability/entities/guard-availability-override.entity.ts
+++ security-backend-nest/src/availability/entities/guard-availability-override.entity.ts
@@ -20,7 +20,7 @@ export class GuardAvailabilityOverride {
   @PrimaryGeneratedColumn()
   id!: number;
 
-  @ManyToOne(() => GuardProfile, { eager: true, onDelete: 'CASCADE' })
+  @ManyToOne(() => GuardProfile, { eager: true, nullable: false, onDelete: 'CASCADE' })
   @JoinColumn({ name: 'guardId' })
   guard!: GuardProfile;
 
