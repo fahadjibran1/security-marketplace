bbe9c2ce4d9484a63b08a42f330021fdec114dc3
Fahad Jibran <fahadjibran@gmail.com>
2026-08-10 13:38:12 +0100

    fix: exclude client portal password hashes from normal queries

--- security-backend-nest/src/client-portal-user/entities/client-portal-user.entity.ts
+++ security-backend-nest/src/client-portal-user/entities/client-portal-user.entity.ts
@@ -23,7 +23,7 @@ export class ClientPortalUser {
   @Column({ type: 'varchar', unique: true })
   email!: string;
 
-  @Column({ type: 'varchar' })
+  @Column({ type: 'varchar', select: false })
   passwordHash!: string;
 
   @Column({ type: 'varchar' })
